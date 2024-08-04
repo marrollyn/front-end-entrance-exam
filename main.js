5998a205aef10846bf2ebfed533c9bd0fb61d843
Marina Moiseenko <marina.moiseenko@list.ru>
2024-08-04 20:34:23 +0300

    first build

---
+++ main.js
@@ -0,0 +1,40 @@
+import html2pdf from "html2pdf.js";
+
+function getPDFResume() {
+  const downloadButton = document.getElementById("download");
+  if (downloadButton) {
+      downloadButton.addEventListener("click", () => {
+          const element = document.querySelector(".resume");
+
+          // Опции для html2pdf
+          const options = {
+              margin: 0.5,
+              filename: "resume.pdf",
+              image: { type: "jpeg", quality: 0.98 },
+              html2canvas: { scale: 2 },
+              jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+          };
+
+          // Генерация PDF
+          html2pdf().from(element).set(options).save();
+      });
+  } else {
+      console.error("Не удается скачать файл, не найден id кнопки скачивания.");
+  }
+}
+
+export function updateRange(rangeInput) {
+  const value =
+      ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) *
+      100;
+  rangeInput.style.setProperty("--value", value + "%");
+}
+
+const rangeInputs = document.querySelectorAll(".slider");
+
+rangeInputs.forEach((input) => {
+  input.addEventListener("input", () => updateRange(input));
+  updateRange(input);
+});
+
+getPDFResume();
