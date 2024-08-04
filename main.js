348cfec64db81a35107ecb5503aae52c48e8434c
Marina Moiseenko <marina.moiseenko@list.ru>
2024-08-04 20:50:04 +0300

    first build

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ import html2pdf from "html2pdf.js";
 function getPDFResume() {
   const downloadButton = document.getElementById("download");
   if (downloadButton) {
-      downloadButton.addEventListener("click", () => {
-          const element = document.querySelector(".resume");
+    downloadButton.addEventListener("click", () => {
+      const element = document.querySelector(".resume");
 
-          // Опции для html2pdf
-          const options = {
-              margin: 0.5,
-              filename: "resume.pdf",
-              image: { type: "jpeg", quality: 0.98 },
-              html2canvas: { scale: 2 },
-              jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-          };
+      // Опции для html2pdf
+      const options = {
+        margin: 0.5,
+        filename: "resume.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+      };
 
-          // Генерация PDF
-          html2pdf().from(element).set(options).save();
-      });
+      // Генерация PDF
+      html2pdf().from(element).set(options).save();
+    });
   } else {
-      console.error("Не удается скачать файл, не найден id кнопки скачивания.");
+    console.error("Не удается скачать файл, не найден id кнопки скачивания.");
   }
 }
 
-export function updateRange(rangeInput) {
+function updateRange(rangeInput) {
   const value =
-      ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) *
-      100;
-  rangeInput.style.setProperty("--value", value + "%");
+    ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) *
+    100;
+  rangeInput.style.background = `linear-gradient(to right, #28d979 ${value}%, transparent ${value}%)`;
 }
 
 const rangeInputs = document.querySelectorAll(".slider");
 
 rangeInputs.forEach((input) => {
   input.addEventListener("input", () => updateRange(input));
-  updateRange(input);
+  updateRange(input); // Инициализируем стиль при загрузке страницы
 });
 
 getPDFResume();
